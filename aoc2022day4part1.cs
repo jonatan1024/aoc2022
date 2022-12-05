a875469b45a974df345e8b2aa153270d98cb08d8
jonatan1024 <jonatan1024@gmail.com>
2022-12-05 12:26:40 +0100

    Create aoc2022day4part1.cs

---
+++ aoc2022day4part1.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            int count = 0;
+            foreach (string line in input)
+            {
+                string[] ranges = line.Split(',');
+                int[] firstRange = ranges[0].Split('-').Select(int.Parse).ToArray();
+                int[] secondRange = ranges[1].Split('-').Select(int.Parse).ToArray();
+                if (firstRange[0] <= secondRange[0] && firstRange[1] >= secondRange[1])
+                {
+                    count++;
+                }
+                else if (secondRange[0] <= firstRange[0] && secondRange[1] >= firstRange[1])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
